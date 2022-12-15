dbe323b2f942575336c070a34994fc51b7a7b8f9
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2022-12-15 11:47:27 +0100

    Adapt tests to 6.4.
    Task #072496

---
+++ tests/test_module.py
@@ -0,0 +1,11 @@
+# This file is part sale_credit_limit_total module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class SaleCreditLimitTotalTestCase(ModuleTestCase):
+    'Test Sale Credit Limit Total module'
+    module = 'sale_credit_limit_total'
+
+del ModuleTestCase
