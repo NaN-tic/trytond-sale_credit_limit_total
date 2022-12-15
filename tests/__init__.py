863c73edc5592be8a8fbbe47686c930006fcf045
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2022-12-15 11:47:27 +0100

    Adapt tests to 6.4.
    Task #072496

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part sale_credit_limit_total module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_sale_credit_limit_total import suite
-
-__all__ = ['suite']
