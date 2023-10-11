50b5671fd28d27b208496cc18833917ac25462c4
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 11:15:22 +0200

    Pyflakes fixes

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,9 +2,10 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.tests.test_tryton import ModuleTestCase
+from trytond.modules.company.tests import CompanyTestMixin
 
 
-class SaleCreditLimitTotalTestCase(ModuleTestCase):
+class SaleCreditLimitTotalTestCase(CompanyTestMixin, ModuleTestCase):
     'Test Sale Credit Limit Total module'
     module = 'sale_credit_limit_total'
 
